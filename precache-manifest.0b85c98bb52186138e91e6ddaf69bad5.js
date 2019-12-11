84d4db950349d3cb44e7080075e35d21e7f0416e
mona mohamed <monaelhoby@yahoo.com>
2019-12-11 18:33:26 +0200

    Updates

---
+++ precache-manifest.0b85c98bb52186138e91e6ddaf69bad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4ceca335ace3eccd46a4a0a2fd20f0",
+    "url": "/monaelhoby/currency_converter_app/index.html"
+  },
+  {
+    "revision": "f74a5623c8326387547e",
+    "url": "/monaelhoby/currency_converter_app/static/css/main.2932aafe.chunk.css"
+  },
+  {
+    "revision": "cc86c2c8f2e4ba4a56b9",
+    "url": "/monaelhoby/currency_converter_app/static/js/2.e1fd20f5.chunk.js"
+  },
+  {
+    "revision": "f74a5623c8326387547e",
+    "url": "/monaelhoby/currency_converter_app/static/js/main.a00ed559.chunk.js"
+  },
+  {
+    "revision": "39fcaa22683ff3b05583",
+    "url": "/monaelhoby/currency_converter_app/static/js/runtime-main.9d80fa99.js"
+  }
+]);
