281dd1dc92d50bbaca40f36a0a27ae6b0b05b30b
mona mohamed <monaelhoby@yahoo.com>
2019-12-11 19:04:18 +0200

    Updates

---
+++ precache-manifest.acfe410ee1736f031a065f670cf55922.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244cae4d7f162c8cd6a0a1d0c00a8332",
+    "url": "/currency_converter_app/index.html"
+  },
+  {
+    "revision": "eacc2f7884a2bd7af8db",
+    "url": "/currency_converter_app/static/css/main.2932aafe.chunk.css"
+  },
+  {
+    "revision": "cc86c2c8f2e4ba4a56b9",
+    "url": "/currency_converter_app/static/js/2.e1fd20f5.chunk.js"
+  },
+  {
+    "revision": "eacc2f7884a2bd7af8db",
+    "url": "/currency_converter_app/static/js/main.b9ba64bc.chunk.js"
+  },
+  {
+    "revision": "cd23c9fe52345f236795",
+    "url": "/currency_converter_app/static/js/runtime-main.b792360f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monaelhoby/currency_converter_app/precache-manifest.0b85c98bb52186138e91e6ddaf69bad5.js"
+  "/currency_converter_app/precache-manifest.acfe410ee1736f031a065f670cf55922.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monaelhoby/currency_converter_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/currency_converter_app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
